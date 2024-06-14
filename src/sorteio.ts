5ec8c1aad0601941c4f03c21a1421086c808012f
Jorge-Barbosa <144363253+Jorge-Barbosa@users.noreply.github.com>
2024-06-13 23:46:04 -0300

    Update sorteio.ts

--- src/sorteio.ts
+++ src/sorteio.ts
@@ -7,3 +7,19 @@ export function sortearNumero(min: number, max: number): number {
 export function sortearNumeroEntre1e100(): number {
     return sortearNumero(1, 100);
 }
+
+export function sortearNumerosUnicos(min: number, max: number, quantidade: number): number[] {
+    if (quantidade > (max - min + 1)) {
+        throw new Error('A quantidade de números únicos solicitada é maior do que o intervalo disponível.');
+    }
+
+    const numerosSorteados: number[] = [];
+    while (numerosSorteados.length < quantidade) {
+        const numero = sortearNumero(min, max);
+        if (!numerosSorteados.includes(numero)) {
+            numerosSorteados.push(numero);
+        }
+    }
+
+    return numerosSorteados;
+}
